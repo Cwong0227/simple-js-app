9dc0111fa259904063663d60e2fa36090b2bdbde
Cwong0227 <74435975+Cwong0227@users.noreply.github.com>
2020-11-30 16:48:21 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 let pokemonList = [{
         name: 'Jigglypuff',
         height: '0.5m',
-        types: 'fairy'
+        types: ['fairy', 'normal']
     },
     {
         name: 'Snorlax',
         height: '2.1m',
-        types: 'normal'
+        types: ['normal']
     },
     {
         name: 'Psyduck',
         height: '0.8m',
-        types: 'water'
+        types: ['water']
     },
 ];
